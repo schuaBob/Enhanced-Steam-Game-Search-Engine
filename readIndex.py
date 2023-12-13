afeb630703e37ecabbbab196cb792423da8b0186
Bob Hua <schua1013@gmail.com>
2023-12-13 17:48:57 -0500

    add comment

--- readIndex.py
+++ readIndex.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     parser.add_argument("parser_type", default="single")
     parser.add_argument("query", default="")
     args = parser.parse_args()
+    # can be "single" or "multi"
     indexReader = MyIndexReader(args.parser_type)
     indexReader.search(args.query)
