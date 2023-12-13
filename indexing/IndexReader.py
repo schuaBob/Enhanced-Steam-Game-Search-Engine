ffee9c991849f6d5bdff17ef3b0cbac4669878c6
Bob Hua <schua1013@gmail.com>
2023-12-13 17:47:44 -0500

    add field booster

--- indexing/IndexReader.py
+++ indexing/IndexReader.py
@@ -1,12 +1,20 @@
 from whoosh import index
 from whoosh.query import *
-from whoosh.qparser import QueryParser, OrGroup
+from whoosh.qparser import QueryParser, OrGroup, MultifieldParser
 class MyIndexReader:
-    def __init__(self):
+    def __init__(self, parser_type):
         path_dir= "index"
         myindex = index.open_dir(path_dir)
         self.searcher = myindex.searcher()
-        self.parser = QueryParser("game_desc", schema=myindex.schema, group=OrGroup)
+        if parser_type == "single":
+            self.parser = QueryParser("game_desc", schema=myindex.schema, group=OrGroup)
+        if parser_type == "multi":
+            fieldsboots = {
+                "game_name":0.3,
+                "game_desc":0.7,
+            }
+            fields = list(fieldsboots.keys())
+            self.parser = MultifieldParser(fields, schema=myindex.schema, group=OrGroup, fieldboosts=fieldsboots)
     def search(self, query:str):
         myquery = self.parser.parse(query.strip().lower())
         result = self.searcher.search(myquery)
--- readIndex.py
+++ readIndex.py
@@ -1,8 +1,9 @@
 from indexing.IndexReader import MyIndexReader
 import argparse
 if __name__ == "__main__":
-    indexReader = MyIndexReader()
     parser = argparse.ArgumentParser()
+    parser.add_argument("parser_type", default="single")
     parser.add_argument("query", default="")
     args = parser.parse_args()
+    indexReader = MyIndexReader(args.parser_type)
     indexReader.search(args.query)
