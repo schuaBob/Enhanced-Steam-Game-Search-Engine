b205fd33e2205792cb5cfcd99975088766857605
Bob Hua <schua1013@gmail.com>
2023-12-13 17:54:37 -0500

    update backend

--- backend.py
+++ backend.py
@@ -3,12 +3,14 @@ from indexing.IndexReader import MyIndexReader
 from flask_cors import CORS
 
 app = Flask(__name__)
-indexReader = MyIndexReader()
 CORS(app)
 
 @app.route("/search")
 def search():
     query = request.args.get('q')
+    # can be "single" or "multi"
+    parser_type = request.args.get('parser_type', "multi")
+    indexReader = MyIndexReader(parser_type=parser_type)
     myquery = indexReader.parser.parse(query.strip().lower())
     result = indexReader.searcher.search(myquery)
     print(f"query: {myquery.all_terms()}")
